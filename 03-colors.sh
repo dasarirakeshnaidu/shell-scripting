5dbf33642d13faf4d2978f968e633d92b752f8e0
Rakesh <dasarirakeshnaidu@gmail.com>
2023-04-15 19:59:06 +0530

    Autopush

---
+++ 03-colors.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Colors       Foreground          Background
+
+# Red               31                  41
+
+# Green             32                  42
+
+# Yellow            33                  43
+
+# Blue              34                  44
+
+# Magenta           35                  45
+
+# Cyan              36                  46
+
+#syntax to print colors is 
+
+# echo -e "\e[COL-CODEm Your msg to be printed \e[0m"
+
+echo -e"\e[32m I am printing Green color \e[0m"
