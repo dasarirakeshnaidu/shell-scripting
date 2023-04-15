13da1d277bac78e7e4c36b2e0de2ae69ca6dabe4
Rakesh <dasarirakeshnaidu@gmail.com>
2023-04-15 20:24:31 +0530

    Autopush

---
+++ 05-dynamic-vars.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+TODAYDATE=$(date +%F)  # This way of declaring is called as hardcoding.
+
+# to see no. of users logged in to the system $ who
+
+
+
+echo -e "Welcome to Bash Training, Today date is \e[32m ${TODAYDATE} \e[0m"
+echo -e "Number of users sessions in the system are : \e[32m  $(who | wc -l) \e[0m"
+
+
