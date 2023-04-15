6e25793bc50cf800a5b766d124088b491a4999c0
Rakesh <dasarirakeshnaidu@gmail.com>
2023-04-15 20:10:56 +0530

    Autopush

---
+++ 04-variables.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# What is a variable??
+#  which holds the value dynamically
+
+a=10
+b=20
+c=30
+
+# No datatypes are available in bash by default
+
+# Everything is considered as String
+
+# How do you print a variable ??
+
+# We use a special character called $ to print
+
+# echo $a or echo $(a) or echo "$a"
+
+echo "I am printing the value of d $d"
