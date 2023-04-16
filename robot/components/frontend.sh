25e90b477e969c411428f313163f2666237a8118
Rakesh <dasarirakeshnaidu@gmail.com>
2023-04-16 22:42:55 +0530

    Autopush

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -12,12 +12,13 @@ fi
 
 stat() {
     if [ $1 -eq 0 ] ; then
-    echo -e "\e[32m success \e[0m"
+         echo -e "\e[32m success \e[0m"
     else 
-     echo -e "\e[31m Failure \e[0m"
+         echo -e "\e[31m Failure \e[0m"
     exit 2
     fi
 }
+
 echo -n "Installing Nginx : "
 yum install nginx -y &>> /tmp/frontend.log
 
