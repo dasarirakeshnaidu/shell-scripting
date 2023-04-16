e0f56069d8b66431c90023198be6abb90b259afc
Rakesh <dasarirakeshnaidu@gmail.com>
2023-04-16 23:25:05 +0530

    Autopush

--- robot/components/mongodb.sh
+++ robot/components/mongodb.sh
@@ -34,8 +34,8 @@ stat $?
 
 
 
-systemctl enable mongod
-systemctl start mongod
+# systemctl enable mongod
+# systemctl start mongod
 
 # ```
 
@@ -75,32 +75,3 @@ systemctl start mongod
 
 
 
-echo -n "Installing Nginx : "
-yum install nginx -y &>> $LOGFILE
-stat $?
-
-echo -n "Downloading the $COMPONENT component :"
-
-curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
-stat $?
-
-echo -n "Performing cleanup of old $COMPONENT content :"
-
-cd /usr/share/nginx/html
-rm -rf *   &>> $LOGFILE
-stat $?
-
-echo -n "Copying the downloaded $COMPONENT content : "
-
-unzip /tmp/$COMPONENT.zip  &>> $LOGFILE
-mv $COMPONENT-main/* .
-mv static/* .
-rm -rf $COMPONENT-main README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
-stat $?
-
-echo -n "Starting the service: "
-
-systemctl enable nginx  &>> $LOGFILE
-systemctl start nginx   &>> $LOGFILE
-stat $?
