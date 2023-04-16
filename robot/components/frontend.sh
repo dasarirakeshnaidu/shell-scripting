7d56500f79268ce8ddca19148642f1dbb632fb84
Rakesh <dasarirakeshnaidu@gmail.com>
2023-04-16 18:52:21 +0530

    Autopush

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -10,37 +10,30 @@ if [ "$ID" -ne 0 ] ; then
     exit 1
 fi
 
+stat() {
+    if [$1 -eq 0] ; then 
+    echo -e "\e[32m success \e[0m"
+    else 
+     echo -e "\e[31m Failure \e[0m"
+    exit 2
+    fi
+}
 echo -n "Installing Nginx : "
 yum install nginx -y &>> /tmp/frontend.log
 
-if [ $? -eq 0 ] ; then
-     echo -e "\e[32m Success \e[0m"
-else 
-     echo -e "\e[31m Failure \e[0m"
-     exit 2
-fi
+stat $?
 
 echo -n "Downloading the frontend component :"
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
-if [ $? -eq 0 ] ; then
-     echo -e "\e[32m Success \e[0m"
-else 
-     echo -e "\e[31m Failure \e[0m"
-     exit 3
-fi
+stat $?
 
 echo -n "Performing cleanup of old frontend content :"
 
 cd /usr/share/nginx/html
 rm -rf *   &>> /tmp/frontend.log
-if [ $? -eq 0 ] ; then
-     echo -e "\e[32m Success \e[0m"
-else 
-     echo -e "\e[31m Failure \e[0m"
-     exit 4
-fi
+stat $?
 
 echo -n "Copying the downloaded frontend content : "
 
@@ -49,25 +42,14 @@ mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
-if [ $? -eq 0 ] ; then
-     echo -e "\e[32m Success \e[0m"
-else 
-     echo -e "\e[31m Failure \e[0m"
-     exit 5
-fi
+stat $?
 
 echo -n "Starting the service: "
 
 systemctl enable nginx  &>> /tmp/frontend.log
 systemctl start nginx   &>> /tmp/frontend.log
 
-if [ $? -eq 0 ] ; then
-     echo -e "\e[32m Success \e[0m"
-else 
-     echo -e "\e[31m Failure \e[0m"
-     exit 6
-fi
-
+stat $?
 # restart nginx
 
 # here there are 3 observations :
