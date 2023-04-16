de0936bfa0bb1fbd1df00190eea230919bef0b08
Rakesh <dasarirakeshnaidu@gmail.com>
2023-04-17 00:50:28 +0530

    Autopush

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -109,36 +109,3 @@ stat $?
 # stat $?
 
 
-echo -n "Installing $COMPONENT :"
-
-yum install -y mongodb-org &>> $LOGFILE
-stat $?
-
-
-echo -n "Starting $COMPONENT :"
-systemctl enable mongod &>> $LOGFILE
-systemctl start mongod  &>> $LOGFILE
-stat $? 
-
-echo -n "Updating the $COMPONENT visibility : "
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-stat $?
-
-echo -n "Performing the Daemon-reload : "
-systemctl daemon-reload   &>> $LOGFILE
-systemctl restart mongod  
-stat $?
-
-echo -n "Downloading the $COMPONENT schema :"
-curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
-stat $?
-
-echo -n "Extracting the $COMPONENT schema : "
-unzip  -o /tmp/$COMPONENT.zip  &>> $LOGFILE
-stat $?
-
-echo -n "Injecting the schema :"
-cd /tmp/$COMPONENT-main
-mongo < catalogue.js   &>> $LOGFILE
-mongo < users.js       &>> $LOGFILE
-stat $
