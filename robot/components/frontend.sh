f4a7ff81f699e58c1c6e193ceed09d343c75bdda
Rakesh <dasarirakeshnaidu@gmail.com>
2023-04-16 17:27:02 +0530

    Autopush

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 yum install nginx -y
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
@@ -13,6 +15,11 @@ rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
 systemctl enable nginx
-systemctl start nginx
+systemctl start nginx 
 
 # restart nginx
+
+# here there are 3 observations :
+# 1) few of the steps are failed here but still my sript got executed : set -e
+# 2) Installation  got failed because I've not validated that I've root privilages
+# 3) The info i would like to provide is like success or failure
