35d9fbc45e23fff602d9fb4aec99e749aca42fcb
Rakesh <dasarirakeshnaidu@gmail.com>
2023-04-17 01:10:32 +0530

    Autopush

--- robot/components/catalogue.sh
+++ robot/components/catalogue.sh
@@ -59,7 +59,7 @@ stat $?
 
 
 echo -n "Updating the systemd file with DB details : "
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' home/$APPUSER/$COMPONENT/systemd.service
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/$APPUSER/$COMPONENT/systemd.service
 mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
 stat $?
 
