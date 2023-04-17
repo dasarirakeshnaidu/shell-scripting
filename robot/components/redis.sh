abbb0f1f6c29e93897edcb373b8cb8a358408de4
Rakesh <dasarirakeshnaidu@gmail.com>
2023-04-17 23:28:10 +0530

    Autopush

--- robot/components/redis.sh
+++ robot/components/redis.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "I am redis"
+COMPONENT=redis
+
+
+echo -n "Configuring the $COMPONENT repo : "
+curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>> $LOGFILE
+stat $? 
+
+echo -n "Installing $COMPONENT server :"
+yum install redis-6.2.11 -y  &>> $LOGFILE
+stat $? 
+
